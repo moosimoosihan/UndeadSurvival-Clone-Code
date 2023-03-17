5e56ba5a4761ca8d0681f3f9ba7952af7d4107cd
moosimoosihan <segun408@gmail.com>
2023-03-17 16:32:07 +0900

    first commit

---
+++ Assets/Script/HUD.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoYType { Exp, Level, Kill, Time, Health }
+    public InfoYType type;
+
+    Text myText;
+    Slider mySlider;
+
+    void Awake()
+    {
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
+    }
+
+    void LateUpdate()
+    {
+        switch(type){
+            case InfoYType.Exp:
+                float curExp = GameManager.instance.exp;
+                float maxExp = GameManager.instance.nextExp[GameManager.instance.level];
+                mySlider.value = curExp/maxExp;
+                break;
+            case InfoYType.Level:
+                myText.text = string.Format("Lv.{0:F0}",GameManager.instance.level);
+                break;
+            case InfoYType.Kill:
+                myText.text = string.Format("{0:F0}",GameManager.instance.kill);
+                break;
+            case InfoYType.Time:
+                float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                int min = Mathf.FloorToInt(remainTime / 60);
+                int sec = Mathf.FloorToInt(remainTime % 60);
+                myText.text = string.Format("{0:D2}:{1:D2}",min, sec);
+                break;
+            case InfoYType.Health:
+                float curHealth = GameManager.instance.health;
+                float maxHealth = GameManager.instance.maxHealth;
+                mySlider.value = curHealth/maxHealth;
+                break;
+        }
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Vector2 inputVec;
+    public Scaner scanner;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+    
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        scanner = GetComponent<Scaner>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed",inputVec.magnitude);
+
+        if(inputVec.x != 0){
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
