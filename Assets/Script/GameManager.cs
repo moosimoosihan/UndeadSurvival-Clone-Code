2d7a59b62e1272fe06eaaf22d098545d7f48459f
moosimoosihan <segun408@gmail.com>
2023-05-14 00:24:22 +0900

    final commit

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -21,11 +21,13 @@ public class GameManager : MonoBehaviour
     public LevelUp uiLevelUp;
     public PoolManager pool;
     public Result uiResult;
+    public Transform uiJoy;
     public GameObject enemyCleaner;
 
     void Awake()
     {
         instance = this;
+        Application.targetFrameRate = 60;
     }
 
     public void GameStart(int id)
@@ -76,6 +78,9 @@ public class GameManager : MonoBehaviour
     public void GameRetry(){
         SceneManager.LoadScene(0);
     }
+    public void GameQuit(){
+        Application.Quit();
+    }
     void Update()
     {
         if(!isLive)
@@ -106,9 +111,11 @@ public class GameManager : MonoBehaviour
     public void Stop(){
         isLive = false;
         Time.timeScale = 0;
+        uiJoy.localScale = Vector3.zero;
     }
     public void Resume(){
         isLive = true;
         Time.timeScale = 1;
+        uiJoy.localScale = Vector3.one;
     }
 }
